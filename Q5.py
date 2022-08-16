b4b16d5f6a53e2ee4fa8a04a0657d0e01f2e5d5c
letstartnow <110475668+letstartnow@users.noreply.github.com>
2022-08-16 15:36:38 +0900

    최소공배수 구하기

--- Q5.py
+++ Q5.py
@@ -1,39 +1,22 @@
 # 1 ~ 20 사이의 어떤 수로도 나누어 떨어지는 가장 작은 수
-#1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
 
-# result = 1
-# for i in range(20,0,-1):
-#     if 
-    
-# 1 3 6 7 8 11 13 15 16 17 19
-# 20
-# 1 2 3 22 5 23 7 222 33 25 11 223 13 27 35 2222 17 233 19 225
+import math
 
+LCM = 1
 
-# result = []
-# pre = []
-# for N in range(10,0,-1):
-#     d = 2
-#     while N != 1:
-#         if N % d != 0:
-#             d += 1
-#         else:
-#             N //= d
-#             pre.append(d)
-#     for i in pre:
-#         if result.count(i) > pre.count(i):
-#             pass
-#         else:
-#             result.append(d)
-#     for a in pre:
-#         result *= a
-        
-# print(result)
+for i in range(1, 21):
+    GCD = math.gcd(i,LCM)
+    LCM = int(i*LCM/GCD)
+    print(i, LCM)
 
 
-result=[]
-for N in range(10,0,-1):
-    d=2
-    while N != 1:
-        if N % d != 0:
-            N
+num = 1
+
+while num > 0:
+    for n in range(1,21):
+        if num % n != 0:
+            num += 1
+            break
+    else:
+        print(num)
+        num = 0
