35801614f34c1aa0dfb61ee02dc3266cff882d85
letstartnow <110475668+letstartnow@users.noreply.github.com>
2022-08-16 11:58:48 +0900

    피타고라스 수

--- Q9.py
+++ Q9.py
@@ -1,5 +1,6 @@
 # a + b + c = 1000 이 되는 피타고라스 수
 
+
 result = 1
 l = range(1,1000)
 for a in l:
@@ -10,4 +11,4 @@ for a in l:
                     result = a*b*c
 print(result)
 
-#22.07.25
+# 22.07.25
